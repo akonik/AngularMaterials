30f8f164daa1076ae204d1f1675cdaae6d55b37d
AlexKonik <alex.konik@softum.net>
2019-09-05 11:40:40 +0300

    small changes

--- src/app/components/resume-facts-charts/resume-facts-radar-chart-component/resume.facts.radar.chat.component.ts
+++ src/app/components/resume-facts-charts/resume-facts-radar-chart-component/resume.facts.radar.chat.component.ts
@@ -3,35 +3,39 @@ import { RadialChartOptions, ChartDataSets, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
 @Component({
-    selector: 'app-resume-facts-radar-chart',
-    templateUrl: 'resume.facts.radar.chat.component.html'
+  selector: 'app-resume-facts-radar-chart',
+  templateUrl: 'resume.facts.radar.chat.component.html'
 })
 export class ResumeFactsRadarChartComponent implements OnInit {
 
 
-    public radarChartOptions: RadialChartOptions = {
-        responsive: true,
-      };
-      public radarChartLabels: Label[] = 
-      [
-          'C#', 
-          'ASP.NET', 
-          'Angular', 
-          'React.JS', 
-          'SQL', 
-          'Unity',
-          'UnrealEngine4'
-        ];
-    
-      public radarChartData: ChartDataSets[] = [
-        { data: [9, 7, 2, 1, 8, 2, 2], label: 'Technologies used in projects' },
-      ];
-      public radarChartType: ChartType = 'radar';
-
-    constructor() { }
-
-    ngOnInit() { 
-
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+    scale: {
+      ticks: {
+        beginAtZero: true,
+        max: 10
+      }
     }
+
+  };
+  public radarChartLabels: Label[] =
+    [
+      'Backend',
+      'Frontend',
+      'Game Dev',
+      'Databases',
+    ];
+
+  public radarChartData: ChartDataSets[] = [
+    { data: [9, 7, 5, 8], label: 'Skills' },
+  ];
+  public radarChartType: ChartType = 'radar';
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 
 }
